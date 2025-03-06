37bacb7e0a3fe13c1250d7d6e6275435a9831462
Lamia <elbo.lamia@gmail.com>
2025-03-06 16:13:17 +0100

    Delete complex_object.js

--- complex_object.js
+++
@@ -1,8 +0,0 @@
-const complexObject = [
-    {"oiseaux": ["canari", "perroquet", "pigeon", "aigle"]},
-    {"poissons": ["baleine", "dauphin", "requin", "nemo"]},
-    {"quadrupedes": ["chat", "lapin", {"bipedes": ["humain", "singe", "ours"]}]},
-    {"insectes": ["fourmi","termite","scarabée","libellule","papillon"]},
-    {"other": [null, null, null, null, null]},
-    {"another": null}
-];
